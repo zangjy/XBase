16b32757ad783285339ab6b5c5745d828f6d03ea
zangjy <2359270784@qq.com>
2022-06-14 15:27:22 +0800

    1.StatusLayoutUtils增加获取当前配置的方法

--- library/src/main/java/com/zjy/xbase/bean/StatusLayoutManagerConfig.kt
+++ library/src/main/java/com/zjy/xbase/bean/StatusLayoutManagerConfig.kt
@@ -7,19 +7,19 @@ package com.zjy.xbase.bean
  * 描述：切换状态布局的配置
  */
 data class StatusLayoutManagerConfig(
-    val defaultLoadingText: String,//加载中布局的提示文本
+    var defaultLoadingText: String,//加载中布局的提示文本
 
-    val defaultEmptyText: String,//空布局中的提示文本
-    val defaultEmptyImg: Int,//空布局的默认图片
-    val defaultEmptyRetryText: String,//空布局的重试文本
-    val defaultEmptyRetryTextColor: Int,//空布局的重试文本颜色
-    val defaultEmptyRetryVisible: Boolean,//空布局中是否隐藏重试按钮
+    var defaultEmptyText: String,//空布局中的提示文本
+    var defaultEmptyImg: Int,//空布局的默认图片
+    var defaultEmptyRetryText: String,//空布局的重试文本
+    var defaultEmptyRetryTextColor: Int,//空布局的重试文本颜色
+    var defaultEmptyRetryVisible: Boolean,//空布局中是否隐藏重试按钮
 
-    val defaultErrorText: String,//出错布局的提示文本
-    val defaultErrorImg: Int,//出错布局的默认图片
-    val defaultErrorRetryText: String,//出错布局的重试文本
-    val defaultErrorRetryTextColor: Int,//出错布局的重试文本颜色
-    val defaultErrorRetryVisible: Boolean,//出错布局中是否隐藏重试按钮
+    var defaultErrorText: String,//出错布局的提示文本
+    var defaultErrorImg: Int,//出错布局的默认图片
+    var defaultErrorRetryText: String,//出错布局的重试文本
+    var defaultErrorRetryTextColor: Int,//出错布局的重试文本颜色
+    var defaultErrorRetryVisible: Boolean,//出错布局中是否隐藏重试按钮
 
-    val defaultLayoutBackgroundColor: Int //默认布局背景，包括加载中、空数据和出错布局
+    var defaultLayoutBackgroundColor: Int //默认布局背景，包括加载中、空数据和出错布局
 )
